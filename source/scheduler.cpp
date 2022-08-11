3dc96f268462e8f2aeaf19829034a1d9571b00ab
Jetstream <bigalex934@gmail.com>
2022-08-11 21:01:59 +0300

    Implement functions scheduler

---
+++ source/scheduler.cpp
@@ -0,0 +1,50 @@
+#include "scheduler.h"
+#include <iostream>
+#include <iomanip>
+#include "event.h"
+
+Scheduler::Scheduler() : _running(false) {
+
+}
+
+void Scheduler::start() {
+    _worker = std::make_unique<std::thread>([this]() {
+        _running = true;
+        while (_running) {
+            std::lock_guard<std::mutex> lock(_mutex);
+            std::for_each(_exec_queue.begin(), _exec_queue.end(), [](Executable &exec) {
+                const auto current_time = std::chrono::steady_clock::now();
+                if (current_time - exec.start_measure >= exec.delay) {
+                    exec.executor();
+                    exec.remove_later = true;
+                }
+            });
+            auto erase_from = std::remove_if(_exec_queue.begin(), _exec_queue.end(), [](const Executable &exec) {
+                return exec.remove_later;
+            });
+            if (erase_from != _exec_queue.end())
+                _exec_queue.erase(erase_from, _exec_queue.end());
+        }
+    });
+}
+
+void Scheduler::stop() {
+    _running = false;
+    if (_worker && _worker->joinable())
+        _worker->join();
+}
+
+void Scheduler::execute(std::function<void()> &&executor, std::chrono::milliseconds &&delay) {
+    std::lock_guard<std::mutex> lock(_mutex);
+    Executable exec;
+    exec.executor = executor;
+    exec.start_measure = std::chrono::steady_clock::now();
+    exec.delay = delay;
+    exec.remove_later = false;
+
+    _exec_queue.emplace_back(std::move(exec));
+}
+
+Scheduler::~Scheduler() {
+    stop();
+}
---
+++ source/scheduler.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <vector>
+#include <thread>
+#include <mutex>
+class Event;
+
+class Scheduler {
+public:
+    explicit Scheduler();
+
+    Scheduler(const Scheduler &other) = delete;
+    Scheduler &operator=(const Scheduler &other) = delete;
+    Scheduler(const Scheduler &&other) = delete;
+    Scheduler &operator=(const Scheduler &&other) = delete;
+
+    void start();
+    void stop();
+    void execute(std::function<void()> &&executor, std::chrono::milliseconds &&delay);
+
+    ~Scheduler();
+private:
+    std::mutex _mutex;
+    std::unique_ptr<std::thread> _worker;
+    bool _running;
+
+    struct Executable {
+        std::function<void()> executor;
+        std::chrono::time_point<std::chrono::steady_clock> start_measure;
+        std::chrono::milliseconds delay;
+        bool remove_later;
+    };
+    std::vector<Executable> _exec_queue;
+};
